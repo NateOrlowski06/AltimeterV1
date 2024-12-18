614ef530980f9488ad572cb141ec48e74a04ffb6
NateOrlowski06 <NateOrlowski@protonmail.com>
2024-12-18 15:25:29 -0500

    Cleaned Accelerometer.py code

--- Accelerometer.py
+++ Accelerometer.py
@@ -2,7 +2,6 @@ import board
 import adafruit_bno055
 import math
 
-
 """
 This is where the accelerometer data is read and processed.
 The BNO055 returns a tuple containing an x, y, and z acceleration reading
@@ -15,34 +14,40 @@ math operations are attempted. A try/except block in the update function
 catches those and replaces them with the most recent data point. 
 """
 
-
-
 WINDOW_SIZE = 10
 METRIC_TO_GRAVITY = 9.81
+
 class Accelerometer():
 
     def __init__(self):
+
         self.i2c = board.I2C()
         self.sensor = adafruit_bno055.BNO055_I2C(self.i2c)
+
         self.x = 0.0
         self.y = 0.0
         self.z = 0.0
         self.acceleration = 0.0
-        self.acceleration_readings = [0.0]
         self.max_acceleration = 0.0
+        self.acceleration_readings = [0.0]
+        
 
     def update(self):
         
         self.x = self.sensor.linear_acceleration[0]
         self.y = self.sensor.linear_acceleration[1]
         self.z = self.sensor.linear_acceleration[2]
-        if len(self.acceleration_readings)>WINDOW_SIZE:
+
+        #Purges earliest acceleration value in the window
+        if len(self.acceleration_readings) > WINDOW_SIZE:
             self.acceleration_readings.pop(0)
             self.acceleration = sum(self.acceleration_readings) / len(self.acceleration_readings) 
 
-        try: #subtract 0.05 from the acceleration because the sensor reads roughly 0.05 sitting still and converts to Gs
-            self.acceleration_readings.append((math.sqrt(self.x**2 + self.y**2 + self.z**2)-0.05)/METRIC_TO_GRAVITY)
+        #subtract 0.05 from the acceleration because the sensor reads roughly 0.05 sitting still
+        try: 
+            self.acceleration_readings.append( (math.sqrt(self.x**2 + self.y**2 + self.z**2)-0.05) / METRIC_TO_GRAVITY )
         except (TypeError, ValueError):
+        #In the case of a null reading, this repeats the most recent value and moves on
             self.acceleration_readings.append(self.acceleration_readings[-1])
         
         if self.acceleration > self.max_acceleration :
