c17e532c68299d3bd354aed75bacd10798569733
NateOrlowski06 <NateOrlowski@protonmail.com>
2024-12-18 15:21:51 -0500

    Cleaned Altimeter.py code

--- Altimeter.py
+++ Altimeter.py
@@ -17,38 +17,35 @@ The maximum height and maximum velocities are evaluated and stored here
 
 METERS_TO_FEET = 3.281
 WINDOW_SIZE = 10
+
 class Altimeter():
     def __init__(self):
         self.max_altitude = 0.0
+        self.max_velocity = 0.0
+        self.max_height = 0.0
         self.altitude = 0.0
         self.velocity = 0.0
-        self.max_velocity = 0.0
-        self.altitude_readings = [0]*10
+        self.altitude_readings = [0]*WINDOW_SIZE
         self.initial_altitude = bmpsensor.readBmp180()[2] * METERS_TO_FEET
-        self.max_height = 0.0
+        
 
 
     def update(self,dt):
         
         self.altitude_readings.append(bmpsensor.readBmp180()[2] * METERS_TO_FEET)
 
+        #Purges the earliest value in the window
         if (len(self.altitude_readings) > WINDOW_SIZE):
             self.altitude_readings.pop(0)
             self.velocity = (self.altitude_readings[-1] - self.altitude_readings[0])  / (dt * WINDOW_SIZE)
+
+        #Sets altitude variable equal to the average altitude over the window
         self.altitude = sum(self.altitude_readings) / len(self.altitude_readings)
-        
-        
 
-        
-
-
-
-
-
-
+        #Updates all maximum values if necessary
         if (self.altitude > self.max_altitude):
             self.max_altitude = self.altitude
-        if (self.velocity > self.max_velocity and len(self.altitude_readings) >= WINDOW_SIZE):
+        if (self.velocity > self.max_velocity):
             self.max_velocity = self.velocity
         if (self.getHeight() > self.max_height):
             self.max_height = self.getHeight()
