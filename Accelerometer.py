39ef2ee0339f242c72fdf759493e70e2647d5fc9
Nate Orlowski <nathaniel.orlowski@gmail.com>
2024-12-18 14:28:34 -0500

    Fixed acceleration offset

--- Accelerometer.py
+++ Accelerometer.py
@@ -41,8 +41,8 @@ class Accelerometer():
             self.acceleration_readings.pop(0)
             self.acceleration = sum(self.acceleration_readings) / len(self.acceleration_readings) 
 
-        try: #subtract 0.5 from the acceleration because the sensor reads roughly 0.5 sitting still
-            self.acceleration_readings.append(math.sqrt(self.x**2 + self.y**2 + self.z**2)-0.5)
+        try: #subtract 0.05 from the acceleration because the sensor reads roughly 0.05 sitting still
+            self.acceleration_readings.append(math.sqrt(self.x**2 + self.y**2 + self.z**2)-0.05)
         except (TypeError, ValueError):
             self.acceleration_readings.append(self.acceleration_readings[-1])
         
